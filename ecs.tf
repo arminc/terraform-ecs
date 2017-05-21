69ea259a0f88f7e5a32607dc6c5c66ff7f6c3aa4
Armin <armin@coralic.nl>
2017-05-21 17:16:10 +0200

    Fix wrong commit in ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,4 +1,4 @@
-/*provider "aws" {
+provider "aws" {
   region = "eu-west-1"
 }
 
@@ -22,13 +22,6 @@ module "ecs" {
 resource "aws_key_pair" "ecs" {
   key_name   = "ecs-key-${var.environment}"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtMljjj0Ccxux5Mssqraa/iHHxheW+m0Rh17fbd8t365y9EwBn00DN/0PjdU2CK6bjxwy8BNGXWoUXiSDDtGqRupH6e9J012yE5kxhpXnnkIcLGjkAiflDBVV4sXS4b3a2LSXL5Dyb93N2GdnJ03FJM4qDJ8lfDQxb38eYHytZkmxW14xLoyW5Hbyr3SXhdHC2/ecdp5nLNRwRWiW6g9OA6jTQ3LgeOZoM6dK4ltJUQOakKjiHsE+jvmO0hJYQN7+5gYOw0HHsM+zmATvSipAWzoWBWcmBxAbcdW0R0KvCwjylCyRVbRMRbSZ/c4idZbFLZXRb7ZJkqNJuy99+ld41 ecs@aws.fake"
-}*/
-
-module "abc" {
-  source = "modules/ecs_events"
-
-  environment = "${var.environment}"
-  cluster     = "${var.environment}"
 }
 
 variable "vpc_cidr" {}
@@ -51,8 +44,6 @@ variable "availibility_zones" {
   type = "list"
 }
 
-/*
 output "default_alb_target_group" {
   value = "${module.ecs.default_alb_target_group}"
-}*/
-
+}
