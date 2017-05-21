88f221b65225763a07d64400a12ffb88f10fa654
Armin <armin@coralic.nl>
2017-05-21 18:21:35 +0200

    Fix hardcoded region

--- modules/ecs_events/main.tf
+++ modules/ecs_events/main.tf
@@ -4,13 +4,17 @@ resource "aws_sns_topic" "ecs_events" {
 
 data "aws_caller_identity" "current" {}
 
+data "aws_region" "current" {
+  current = true
+}
+
 data "template_file" "ecs_task_stopped" {
   template = <<EOF
 {
   "source": ["aws.ecs"],
   "detail-type": ["ECS Task State Change"],
   "detail": {
-    "clusterArn": ["arn:aws:ecs:eu-west-1:$${account_id}:cluster/$${cluster}"],
+    "clusterArn": ["arn:aws:ecs:$${aws_region}:$${account_id}:cluster/$${cluster}"],
     "lastStatus": ["STOPPED"],
     "stoppedReason": ["Essential container in task exited"]
   }
@@ -20,6 +24,7 @@ EOF
   vars {
     account_id = "${data.aws_caller_identity.current.account_id}"
     cluster    = "${var.cluster}"
+    aws_region = "${data.aws_region.current.name}"
   }
 }
 
