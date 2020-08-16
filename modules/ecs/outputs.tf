97d9fce02b1a44a8bad7fbb3dcfdbfa055549d9a
Theo Gravity <theo@suteki.nu>
2020-08-15 19:42:06 -0700

    Add cluster_id output

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,3 +1,7 @@
 output "default_alb_target_group" {
   value = module.alb.default_alb_target_group
 }
+
+output "cluster_id" {
+  value = aws_ecs_cluster.cluster.id
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "default_alb_target_group" {
   value = module.ecs.default_alb_target_group
 }
+
+output "cluster_id" {
+  value = module.ecs.cluster_id
+}
