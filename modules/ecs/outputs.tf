029dea874df1867c9c7e8099bf22ed69a5ace1d7
Theo Gravity <theo@suteki.nu>
2020-08-15 22:48:04 -0700

    Add private_subnet_ids, public_subnet_ids, vpc_id

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -9,3 +9,15 @@ output "cluster_id" {
 output "instance_security_group_id" {
   value = module.ecs_instances.ecs_instance_security_group_id
 }
+
+output "private_subnet_ids" {
+  value = module.network.private_subnet_ids
+}
+
+output "public_subnet_ids" {
+  value = module.network.public_subnet_ids
+}
+
+output "vpc_id" {
+  value = module.network.vpc_id
+}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "cluster_id" {
 output "instance_security_group_id" {
   value = module.ecs.instance_security_group_id
 }
+
+output "private_subnet_ids" {
+  value = module.ecs.private_subnet_ids
+}
+
+output "public_subnet_ids" {
+  value = module.ecs.public_subnet_ids
+}
+
+output "vpc_id" {
+  value = module.ecs.vpc_id
+}
