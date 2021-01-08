0602ae7baa196a1ff9076cdfe3380539b670ec28
justin ross <ross.justin.t@gmail.com>
2021-01-08 11:55:08 -0800

    Fix typos.

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -18,12 +18,12 @@ variable "vpc_cidr" {
 
 variable "private_subnet_cidrs" {
   type        = list
-  description = "List of private cidrs, for every avalibility zone you want you need one. Example: 10.0.0.0/24 and 10.0.1.0/24"
+  description = "List of private cidrs, for every availability zone you want you need one. Example: 10.0.0.0/24 and 10.0.1.0/24"
 }
 
 variable "public_subnet_cidrs" {
   type        = list
-  description = "List of public cidrs, for every avalibility zone you want you need one. Example: 10.0.0.0/24 and 10.0.1.0/24"
+  description = "List of public cidrs, for every availability zone you want you need one. Example: 10.0.0.0/24 and 10.0.1.0/24"
 }
 
 variable "load_balancers" {
@@ -34,7 +34,7 @@ variable "load_balancers" {
 
 variable "availability_zones" {
   type        = list
-  description = "List of avalibility zones you want. Example: eu-west-1a and eu-west-1b"
+  description = "List of availability zones you want. Example: eu-west-1a and eu-west-1b"
 }
 
 variable "max_size" {
