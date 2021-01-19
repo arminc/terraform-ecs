daa75ec61a84340a83edeadf93140ee4ae44364b
justin ross <ross.justin.t@gmail.com>
2021-01-18 16:50:34 -0800

    Omit unused lines from user_data.sh template.

--- modules/ecs_instances/templates/user_data.sh
+++ modules/ecs_instances/templates/user_data.sh
@@ -91,16 +91,6 @@ EOF
 
 start ecs
 
-# Get ECS instance info, although not used in this user_data itself, this allows you to use
-# az(availability zone) and region
-until $(curl --output /dev/null --silent --head --fail http://localhost:51678/v1/metadata); do
-  printf '.'
-  sleep 5
-done
-instance_arn=$(curl -s http://localhost:51678/v1/metadata | jq -r '. | .ContainerInstanceArn' | awk -F/ '{print $NF}' )
-az=$(curl -s http://instance-data/latest/meta-data/placement/availability-zone)
-region=$${az:0:$${#az} - 1}
-
 # Custom userdata script code
 ${custom_userdata}
 
